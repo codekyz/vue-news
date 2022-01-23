01965796724172835ea729da3437c054e84d5d4b
codekyz <codekyz0111@gmail.com>
2022-01-23 19:37:10 +0900

    :construction: Router basic
    Router basic

---
+++ src/components/ToolBar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="header">
+      <router-link to="/news">News</router-link> |
+      <router-link to="/ask">Ask</router-link> |
+      <router-link to="/jobs">Jobs</router-link>
+  </div>
+</template>
+
+<style scoped>
+.header {
+    color: #FFF;
+    background-color: #42B883;
+    display: flex;
+    padding: 8px;
+}
+
+.header a {
+    color: #FFF;
+}
+
+.header .router-link-exact-active {
+    color: #35495E;
+}
+
+</style>
--- src/views/ItemView.vue
+++ src/views/ItemView.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-      jobs
+      Item
   </div>
 </template>
 
